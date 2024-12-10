973a87dd059962735231c5e40ea358e8606038a6
Perfect Aimers Enterprise <perfectaimersenterprise@gmail.com>
2024-12-10 08:47:25 +0100

    dovees

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express');
 const cors = require('cors')
 const http = require('http');
+const path = require('path')
 const { Server } = require('socket.io');
 
 const app = express();
