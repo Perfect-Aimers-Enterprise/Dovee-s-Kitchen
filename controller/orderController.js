2457a5dd071604eca97354233d544c1af3f118a5
Perfect Aimers Enterprise <perfectaimersenterprise@gmail.com>
2025-09-04 09:29:21 +0700

    addging gmail

--- controller/orderController.js
+++ controller/orderController.js
@@ -45,7 +45,7 @@ const createProceedOrder = async (req, res) => {
 
         const transporter = nodemailer.createTransport({
             host: "smtp.zoho.com",
-            port: 587,
+            port: 465,
             secure: true,
             auth: {
                 user: process.env.DOVEEYS_EMAIL,
