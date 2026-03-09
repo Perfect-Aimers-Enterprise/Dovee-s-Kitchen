e60695f6fd617f2779fe3af6081ec972b56c1caa
Perfect Aimers Enterprise <perfectaimersenterprise@gmail.com>
2026-03-09 20:45:05 +0100

    doveeys kitchen cloud storage

---
+++ app.js
@@ -0,0 +1,81 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const { createServer } = require("http");
+const bodyParser = require("body-parser");
+const path = require("path");
+const connectDB = require("./db/connectDB");
+const userRoute = require("./routes/userRoute");
+const productRoute = require("./routes/productRoute");
+const specialProductRoute = require("./routes/specialProductRoute");
+const orderRoute = require("./routes/orderRoute");
+const adminGetOrderRoute = require("./routes/adminGetOrderRoute");
+const userMessageRoute = require("./routes/userMessageRoute");
+const subscribeRoute = require("./routes/subscriptionRoute");
+const landingSectionRoute = require("./routes/landingSectionRoute");
+const galleryRoute = require("./routes/galleryRoute");
+const dailyMenuRoute = require("./routes/dailyMenuRoute");
+const eventMgtRoute = require("./routes/eventMgtRoute");
+const adminSecureRoute = require("./routes/adminAuthRoute");
+const eventHeaderRoute = require("./routes/eventHeaderRoute");
+const toggleEventRoute = require("./routes/toggleEventRoute");
+
+const app = express();
+const server = createServer(app);
+
+// Update Express CORS to match
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  }),
+);
+
+app.use(express.json());
+
+// Health check / root route
+// app.get("/", (req, res) => {
+//   res.status(200).send("This server is live");
+// });
+
+// app.use("/v1", routes);
+
+app.use(bodyParser.json());
+app.use(
+  express.static(path.join(__dirname, "public"), {
+    setHeaders: (res, fiiePath) => {
+      res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+      res.setHeader("Pragma", "no-cache");
+      res.setHeader("Expires", "0");
+      res.setHeader("Surrogate-Control", "no-store");
+    },
+  }),
+);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/htmlFolder/Doviee2.html");
+});
+
+app.get("/doveeyskitchenadmin", (req, res) => {
+  res.sendFile(__dirname + "/public/htmlFolder/adminAuth.html");
+});
+
+// MiddleWares
+app.use("/doveeysKitchen/api", userRoute);
+app.use("/doveeysKitchen/product", productRoute);
+app.use("/doveeysKitchen/order", orderRoute);
+app.use("/doveeysKitchen/adminGetOrder", adminGetOrderRoute);
+app.use("/doveeysKitchen/specialProduct", specialProductRoute);
+app.use("/doveeysKitchen/message", userMessageRoute);
+app.use("/notification", subscribeRoute);
+app.use("/doveeysLanding", landingSectionRoute);
+app.use("/galleryDisplay", galleryRoute);
+app.use("/dailyMenuDisplay", dailyMenuRoute);
+app.use("/doveeysKitchen/eventapi", eventMgtRoute);
+app.use("/doveeysKitchen/safezone", adminSecureRoute);
+app.use("/doveeysKitchen/eventHeader", eventHeaderRoute);
+app.use("/doveeysKitchen/eventStatus", toggleEventRoute);
+
+module.exports = { server };
--- server.js
+++ server.js
@@ -1,124 +1,78 @@
-require('dotenv').config()
-const express = require('express');
-const cors = require('cors')
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const { server } = require("./app");
 // const http = require('http');
-const bodyParser = require('body-parser')
-const path = require('path')
-const connectDB = require('./db/connectDB')
-const userRoute = require('./routes/userRoute')
-const productRoute = require('./routes/productRoute')
-const specialProductRoute = require('./routes/specialProductRoute')
-const orderRoute = require('./routes/orderRoute')
-const adminGetOrderRoute = require('./routes/adminGetOrderRoute')
-const userMessageRoute = require('./routes/userMessageRoute')
-const subscribeRoute = require('./routes/subscriptionRoute')
-const landingSectionRoute = require('./routes/landingSectionRoute')
-const galleryRoute = require('./routes/galleryRoute')
-const dailyMenuRoute = require('./routes/dailyMenuRoute')
-const eventMgtRoute = require('./routes/eventMgtRoute')
-const adminSecureRoute = require('./routes/adminAuthRoute')
-const eventHeaderRoute = require('./routes/eventHeaderRoute')
-const toggleEventRoute = require('./routes/toggleEventRoute')
+const bodyParser = require("body-parser");
+const path = require("path");
+const connectDB = require("./db/connectDB");
+const userRoute = require("./routes/userRoute");
+const productRoute = require("./routes/productRoute");
+const specialProductRoute = require("./routes/specialProductRoute");
+const orderRoute = require("./routes/orderRoute");
+const adminGetOrderRoute = require("./routes/adminGetOrderRoute");
+const userMessageRoute = require("./routes/userMessageRoute");
+const subscribeRoute = require("./routes/subscriptionRoute");
+const landingSectionRoute = require("./routes/landingSectionRoute");
+const galleryRoute = require("./routes/galleryRoute");
+const dailyMenuRoute = require("./routes/dailyMenuRoute");
+const eventMgtRoute = require("./routes/eventMgtRoute");
+const adminSecureRoute = require("./routes/adminAuthRoute");
+const eventHeaderRoute = require("./routes/eventHeaderRoute");
+const toggleEventRoute = require("./routes/toggleEventRoute");
 
-const authentication = require('./middleWare/authentication')
+const authentication = require("./middleWare/authentication");
 // const { Server } = require('socket.io');
 
 const app = express();
 
-// const http = require('http');
-// const { Server } = require('socket.io');
-// const server = http.createServer(app);
-// const io = new Server(server);
-
 // Middleware to set cache control headers
-app.use((req, res, next) => {
-  // Set the Cache-Control headers to 'no-store' to prevent caching
-  res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
-  res.setHeader('Pragma', 'no-cache');
-  res.setHeader('Expires', '0');
-  next();
-});
 
-
+// app.use((req, res, next) => {
+//   res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+//   res.setHeader('Pragma', 'no-cache');
+//   res.setHeader('Expires', '0');
+//   next();
+// });
 
 // Serve static files
 
+// app.use(cors());
 
-app.use(cors());
+// app.use(express.json())
 
-app.use(express.json())
+// routings
 
-app.use(bodyParser.json())
-app.use(express.static(path.join(__dirname, 'public'), {
-  setHeaders: (res, fiiePath) => {
-    res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
-    res.setHeader('Pragma', 'no-cache');
-    res.setHeader('Expires', '0');
-    res.setHeader('Surrogate-Control', 'no-store')
-  }
-}));
-
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/htmlFolder/Doviee2.html');
-});
-
-app.get('/doveeyskitchenadmin', (req, res) => {
-  res.sendFile(__dirname + '/public/htmlFolder/adminAuth.html');
-});
-
-// MiddleWares 
-app.use('/doveeysKitchen/api', userRoute)
-app.use('/doveeysKitchen/product', productRoute)
-app.use('/doveeysKitchen/order', orderRoute)
-app.use('/doveeysKitchen/adminGetOrder', adminGetOrderRoute)
-app.use('/doveeysKitchen/specialProduct', specialProductRoute)
-app.use('/doveeysKitchen/message', userMessageRoute)
-app.use('/notification', subscribeRoute)
-app.use('/doveeysLanding', landingSectionRoute)
-app.use('/galleryDisplay', galleryRoute)
-app.use('/dailyMenuDisplay', dailyMenuRoute)
-app.use('/doveeysKitchen/eventapi', eventMgtRoute)
-app.use('/doveeysKitchen/safezone', adminSecureRoute)
-app.use('/doveeysKitchen/eventHeader', eventHeaderRoute)
-app.use('/doveeysKitchen/eventStatus', toggleEventRoute)
-
-
-const fs = require('fs');
-const logStream = fs.createWriteStream(__dirname + '/logs/app.log', { flags: 'a' });
+// Use this to log errors
+const fs = require("fs");
+const logStream = fs.createWriteStream(__dirname + "/logs/app.log", { flags: "a" });
 
 app.use((req, res, next) => {
   logStream.write(`[${new Date().toISOString()}] ${req.method} ${req.url}\n`);
   next();
 });
 
-// Use this to log errors
-process.on('uncaughtException', (err) => {
+process.on("uncaughtException", (err) => {
   logStream.write(`[${new Date().toISOString()}] Uncaught Exception: ${err.message}\n`);
 });
 
-
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI, {
       useNewUrlParser: true,
-      useUnifiedTopology: true
-    })
-    app.listen(port, () => {
+      useUnifiedTopology: true,
+    });
+    server.listen(port, () => {
       console.log(`server is listening on port ${port}`);
-
-    })
+    });
   } catch (error) {
-    console.log('error listening to port', error);
-
+    console.log("error listening to port", error);
   }
-}
+};
 
-start()
-
-
+start();
 
 // WebSocket logic
 
